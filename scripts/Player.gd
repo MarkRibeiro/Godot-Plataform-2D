649da05ac7096d6832655f61aca0a1b0db449b40
Mark Ribeiro <40504056+MarkRibeiro@users.noreply.github.com>
2023-10-12 01:20:32 -0300

    atualiza projeto

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,8 +7,15 @@ const JUMP_FORCE = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
+var is_hurted := false
+@export var player_life := 10
+var knockback_vector := Vector2.ZERO
+var direction
 
 @onready var animation := $anim as AnimatedSprite2D
+@onready var remote_transform := $remote as RemoteTransform2D
+@onready var ray_right := $ray_right as RayCast2D
+@onready var ray_left := $ray_left as RayCast2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -24,17 +31,83 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction
-		if !is_jumping:
-			animation.play("run")
-	elif is_jumping:
-		animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("idle")
 
+	if knockback_vector != Vector2.ZERO:
+		velocity = knockback_vector
+	
+	_set_stage()
 	move_and_slide()
+	
+	for platforms in get_slide_collision_count():
+		var collision = get_slide_collision(platforms)
+		if collision.get_collider().has_method("has_collided_with"):
+			collision.get_collider().has_collided_with(collision, self)
+		
+
+
+func _on_hurtbox_body_entered(body):
+	if player_life <= 0:
+		queue_free()
+	else:
+		if ray_right.is_colliding():
+			take_damage(Vector2(-200,-200), 0.25)
+		elif ray_left.is_colliding():
+			take_damage(Vector2(200,-200), 0.25)
+
+func follow_camera(camera):
+	var camera_path = camera.get_path()
+	remote_transform.remote_path = camera_path
+
+func take_damage(knockback_force, duration):
+	player_life -= 1
+	
+	if knockback_force != Vector2.ZERO:
+		knockback_vector = knockback_force
+		
+		var knockback_tween := get_tree().create_tween()
+		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
+		animation.modulate = Color(1, 0, 0, 1)
+		knockback_tween.parallel().tween_property(animation, "modulate", Color(1, 1, 1, 1), duration)
+	
+	is_hurted = true
+	await get_tree().create_timer(.3).timeout
+	is_hurted = false
+
+func _set_stage():
+	var state = "idle"
+	
+	if !is_on_floor():
+		state="jump"
+	elif direction != 0:
+		state = "run"
+	
+	if is_hurted:
+		state = "hurt"
+		
+	if animation.name != state:
+		animation.play(state)
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_FORCE
+			is_jumping = true
+		elif is_on_floor():
+			is_jumping = false
+
+
+func _on_head_collider_body_entered(body):
+	if body.has_method("break_sprite"):
+		body.hitpoints -= 1
+		if body.hitpoints < 0:
+			body.break_sprite()
+		else:
+			body.animation_player.play("hit_flash")
+			body.create_coin()
